1ccd5fb20d7eb655ace9606fe1a1c20b74a02ed1
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2025-10-08 22:39:10 -0300

    consertando semáforo

--- aula-02-css-modules-global/src/pages/index.js
+++ aula-02-css-modules-global/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import TrafficLight from "@/components/TraficLight";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,7 @@ export default function Home() {
       </Head>
 
       <main>
-        
+        <TrafficLight/>
       </main>
 
     </>
