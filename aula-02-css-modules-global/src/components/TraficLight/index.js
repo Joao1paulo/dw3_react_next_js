04b763628eb78b2ee4c934f76e06d2c3c80b4b9d
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2025-10-14 20:11:06 -0300

    aula 03: Consumo de API com AXIOS

--- aula-02-css-modules-global/src/components/TraficLight/index.js
+++ aula-02-css-modules-global/src/components/TraficLight/index.js
@@ -4,26 +4,47 @@ import styles from "@/components/TraficLight/TrafficLight.module.css"
 // Importando o useState
 import { useState } from "react";
 
-
+import styles from "@/components/TrafficLight/TrafficLight.module.css";
+ 
+import { useState } from "react";
+ 
 const TrafficLight = () => {
-
-    // Criando o estado para cor
-    const [color, setColor] = useState("gray");
-    return (
-
-        <>
-            <div className={styles.container}>
-                <h3 style={{ marginTop: "30px" }}>Semáfro da FATEC: </h3>
-                <br />
-                <div className={`${styles.light} ${color == "red" ? styles.red : styles.gray}`}></div>
-                <div className={`${styles.light} ${color == "yellow" ? styles.yellow : styles.gray}`}></div>
-                <div className={`${styles.light} ${color == "green" ? styles.green : styles.gray}`}></div>
-            </div>
-            <button className="button" onClick={() => setColor("red")}>Pare!</button>
-            <button className="button" onClick={() => setColor("yellow")}>Atenção!</button>
-            <button className="button" onClick={() => setColor("green")}>Prossiga</button>
-        </>
-    )
-}
+  const [color, setColor] = useState("gray");
+  return (
+    <>
+      <div className={styles.container}>
+        <h3 style={{ marginTop: "30px" }}>Semáfro da FATEC: </h3>
+ 
+        <div
+          className={`${styles.light} ${
+            color == "red" ? styles.red : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("red")}>
+            Pare!
+          </button>
+        </div>
+        <div
+          className={`${styles.light} ${
+            color == "yellow" ? styles.yellow : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("yellow")}>
+            Atenção!
+          </button>
+        </div>
+        <div
+          className={`${styles.light} ${
+            color == "green" ? styles.green : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("green")}>
+            Prossiga
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default TrafficLight;
